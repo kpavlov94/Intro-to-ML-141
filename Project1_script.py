3653330e6e26ceb24246a604072dad06659653a0
YoussefDTU <114087491+YoussefDTU@users.noreply.github.com>
2022-09-28 13:27:40 +0200

    Update Project1_script.py

--- Project1_script.py
+++ Project1_script.py
@@ -1,45 +1,111 @@
 import numpy as np
 import pandas as pd
-from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show, boxplot, xticks, subplot, yticks
 from scipy.linalg import svd
 
 filename = 'data.csv'
-
 df = pd.read_csv(filename)
 
 raw_data = df.values  
 cols = range(0, 11) 
 X = raw_data[:, cols]
-
 attributeNames = np.asarray(df.columns[cols])
 
 classLabels = raw_data[:,-1]
-
 classNames = np.unique(classLabels)
-
 classDict = dict(zip(classNames,range(len(classNames))))
 y = np.array([classDict[cl] for cl in classLabels])
-
 N, M = X.shape
-
 C = len(classNames)
 
-N1 = np.delete(X,5,1)
-N2 = np.delete(N1,9,1)
-print(N2)
-B, D = N2.shape
 
-y1 = N2 - np.ones((B, 1))*N2.mean(0)
+Xn = np.delete(X,[0,5,10],1)
+B, D = Xn.shape
+Xn = Xn.astype(float)
+attributeNamesXn=np.delete(attributeNames,[0,5,10])
+y1 = Xn - np.ones((B, 1))*Xn.mean(0)
+y1 = y1*(1/np.std(y1,0))
 
-# subtract the mean from the data and divide by the attribute standard
-# deviation to obtain a standardized dataset:
-y2 = N2 + np.ones((B, 1))*N2.mean(0)
-y2 = y2*(1/np.std(y2,0))
-# here were utilizing the broadcasting of a row vector to fit the dimensions 
-# of y2
+boxplot(y1)
+xticks(range(1,9),attributeNamesXn)
+show()
 
-# store the two in a cell, so we can just loop over them:
-# ys = [y1, y2]
-# titles = ['zero-mean', 'zero-mean and unit variance']
-# threshold = 0.9
+Attributes = [1,2,3,4,5,6,7]
+NumAtr = len(Attributes)
 
+figure(figsize=(12,12))
+for m1 in range(NumAtr):
+    for m2 in range(NumAtr):
+        subplot(NumAtr, NumAtr, m1*NumAtr + m2 + 1)
+        for c in range(C):
+            class_mask = (y==c)
+            plot(Xn[class_mask,Attributes[m2]], Xn[class_mask,Attributes[m1]], '.')
+            if m1==NumAtr-1:
+                xlabel(attributeNames[Attributes[m2]])
+            else:
+                xticks([])
+            if m2==0:
+                ylabel(attributeNames[Attributes[m1]])
+            else:
+                yticks([])
+legend(classNames)
+show()
+
+# Subtract mean value from data
+Y = Xn - np.ones((B,1))*Xn.mean(axis=0)
+# PCA by computing SVD of Y
+U,S,Vh = svd(Y,full_matrices=False)
+# Compute variance explained by principal components
+rho = (S*S) / (S*S).sum() 
+threshold = 0.9
+
+plt.figure()
+plt.plot(range(1,len(rho)+1),rho,'x-')
+plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-')
+plt.plot([1,len(rho)],[threshold, threshold],'k--')
+plt.title('Variance explained by principal components');
+plt.xlabel('Principal component');
+plt.ylabel('Variance explained');
+plt.legend(['Individual','Cumulative','Threshold'])
+plt.grid()
+plt.show()
+
+# scipy.linalg.svd returns "Vh", which is the Hermitian (transpose)
+# of the vector V. So, for us to obtain the correct V, we transpose:
+V = Vh.T    
+# Project the centered data onto principal component space
+Z = Y @ V
+# Indices of the principal components to be plotted
+i = 0
+j = 1
+
+# Plot PCA of the data
+f = figure()
+title('Heart Disease: PCA')
+#Z = array(Z)
+for c in range(C):
+    # select indices belonging to class c:
+    class_mask = y==c
+    plot(Z[class_mask,i], Z[class_mask,j], 'o', alpha=.5)
+legend(classNames)
+xlabel('PC{0}'.format(i+1))
+ylabel('PC{0}'.format(j+1))
+show()
+
+# We saw in 2.1.3 that the first 3 components explaiend more than 90
+# percent of the variance. Let's look at their coefficients:
+pcs = [0,1,2]
+legendStrs = ['PC'+str(e+1) for e in pcs]
+c = ['r','g','b']
+bw = .2
+r = np.arange(1,D+1)
+for i in pcs:    
+    plt.bar(r+i*bw, V[:,i], width=bw)
+plt.xticks(r+bw, attributeNamesXn)
+plt.xlabel('Attributes')
+plt.ylabel('Component coefficients')
+plt.legend(legendStrs)
+plt.grid()
+plt.title('NanoNose: PCA Component Coefficients')
+plt.show()
